a34ba6c49d3c4b4361ff2dd2a043a7c1e29c740a
yjlee <dbwlssla87@google.com>
2025-08-08 16:24:22 +0900

    카드 UI 재정렬

--- features/Home/Home.tsx
+++ features/Home/Home.tsx
@@ -3,14 +3,10 @@ import CardListComponent from "../../components/Cards/CardList";
 import WildCardListComponent from "../../components/Cards/WildCardList";
 const Home = () => {
 	return (
-		<>
-			<div className="flex">
-				<div className="mr-5">
-					<CardListComponent />
-				</div>
-				<div><WildCardListComponent /></div>
-			</div>
-		</>
+		<div>
+			<div><CardListComponent /></div>
+			<div><WildCardListComponent /></div>
+		</div>
 	);
 };
 
