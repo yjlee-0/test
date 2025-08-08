941fa608376315372321809c1805ca503a3ff8b6
yjlee <dbwlssla87@google.com>
2025-08-08 16:48:56 +0900

    <></> 삭제

--- components/Cards/WildCard.tsx
+++ components/Cards/WildCard.tsx
@@ -18,19 +18,17 @@ interface WildCardComponentProps {
 const WildCardComponent = ({ imagesrc, imagetitle, imagewidth, imageheight, badge, hashtag, title, price }: WildCardComponentProps) => {
 
 	return (
-		<>
-			<div className='flex justify-between w-[900px] h-[256px] shadow-[0px_0px_6px_#11111126]'>
-				<div className='w-[430px] rounded-[10px] h-[256px] overflow-hidden'>
-					<HeroImage src={imagesrc} alt={imagetitle} width={imagewidth} height={imageheight} />
-				</div>
-				<div className='justify-start p-[30px]'>
-					<Badge label={badge} />
-					<Title label={title} />
-					<HashTag label={hashtag} />
-					<Price price={price} />
-				</div>
+		<div className='flex justify-between w-[900px] h-[256px] shadow-[0px_0px_6px_#11111126]'>
+			<div className='w-[430px] rounded-[10px] h-[256px] overflow-hidden'>
+				<HeroImage src={imagesrc} alt={imagetitle} width={imagewidth} height={imageheight} />
 			</div>
-		</>
+			<div className='justify-start p-[30px]'>
+				<Badge label={badge} />
+				<Title label={title} />
+				<HashTag label={hashtag} />
+				<Price price={price} />
+			</div>
+		</div>
 	)
 }
 
