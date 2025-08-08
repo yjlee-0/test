9782dc3d176c2159d3eb6306eca515d0e5f1b9ca
yjlee <dbwlssla87@google.com>
2025-08-08 16:48:13 +0900

    <></> 삭제

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,9 @@
 const Footer = () => {
-	return <><footer className="inline-flex pt-32 pb-32">
-		<p className='text-3xl font-bold'>footer footer footer</p></footer></>
+	return (
+		<footer className="inline-flex pt-32 pb-32">
+			<p className='text-3xl font-bold'>footer footer footer</p>
+		</footer>
+	)
 }
 
 export default Footer;
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,21 +1,23 @@
 
 import { useRouter } from 'next/router';
+
 const Header = () => {
 	const router = useRouter();
-	return <> <header className='inline-flex pt-32 pb-32'>
-		<div className='text-3xl font-bold'>header header header</div>
-
-		<div className="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
-			<ul>
-				<li className="me-2" onClick={() => router.push('/test')}>
-					test1
-				</li>
-				<li className="me-2" onClick={() => router.push('/test2')}>
-					test2
-				</li>
-			</ul>
-		</div>
-	</header></>
+	return (
+		<header className='inline-flex pt-32 pb-32'>
+			<div className='text-3xl font-bold'>header header header</div>
+			<div className="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
+				<ul>
+					<li className="me-2" onClick={() => router.push('/test')}>
+						test1
+					</li>
+					<li className="me-2" onClick={() => router.push('/test2')}>
+						test2
+					</li>
+				</ul>
+			</div>
+		</header>
+	)
 }
 
 export default Header;
